71f064d69ebeabd880daeb0125e9fc002f9c50ae
maximefromont <maxime.fromont0@gmail.com>
2023-09-11 10:58:38 +0200

    tp1_1_looped.py works with all the fragments

--- tp1_1_looped.py
+++ tp1_1_looped.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 import numpy as np
+import time as time
+
+time_start = time.time()
 
 target_resolution: tuple[int] = 1707, 775
 background_color: tuple[int] = 128, 128, 128
@@ -18,6 +21,8 @@ def rotate_image(image, theta):
 background = np.zeros((*target_resolution[::-1], 4), dtype=np.uint8)
 background += np.array([*background_color, 0], dtype=np.uint8)
 
+fragment_counter = 0
+
 with open(fragments_text_file) as file:
     line: str
     for line in file:
@@ -35,18 +40,22 @@ with open(fragments_text_file) as file:
         # Compute area to override
         fragment_size_y, fragment_size_x = fragment.shape[0], fragment.shape[1]
         x_top_left_corner_pos = x_center_pos - fragment_size_x // 2
-        x_bot_right_corner_pos = x_center_pos + fragment_size_x // 2 + fragment_size_x % 2
+        x_bot_right_corner_pos = x_center_pos + fragment_size_x // 2
         y_top_left_corner_pos = y_center_pos - fragment_size_y // 2
-        y_bot_right_corner_pos = y_center_pos + fragment_size_y // 2 + fragment_size_y % 2
+        y_bot_right_corner_pos = y_center_pos + fragment_size_y // 2
 
         # Paste fragment according to the mask in the alpha channel
-        try:
-            for i in range (x_top_left_corner_pos, x_bot_right_corner_pos):
-                for j in range (y_top_left_corner_pos, y_bot_right_corner_pos):
-                    if fragment[j-y_top_left_corner_pos, i-x_top_left_corner_pos, 3] > 0:
+        for i in range (x_top_left_corner_pos, x_bot_right_corner_pos):
+            for j in range (y_top_left_corner_pos, y_bot_right_corner_pos):
+                if fragment[j-y_top_left_corner_pos, i-x_top_left_corner_pos, 3] > 0:
+                    if(i > 0 and i < target_resolution[0] and j > 0 and j < target_resolution[1]):
                         background[j, i, :] = fragment[j-y_top_left_corner_pos, i-x_top_left_corner_pos, :]
-        except Exception as e:
-            print(index + " " + str(e))
+
+        print(fragment_counter)
+        fragment_counter += 1
+
+time_end = time.time()
+print("Time elapsed: ", time_end - time_start)
 
 cv.imshow(" ", background)
 cv.waitKey(0)
